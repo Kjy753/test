ee9022e3a4ef8e99d3a715a5d2252fc9dc7ec03b
kjy <aty753@naver.com>
2019-07-10 17:27:59 +0900

    출발위치,도착위치 설정 완료,스피너 구현.

--- app/src/main/java/com/example/waytowork/Itemadd.java
+++ app/src/main/java/com/example/waytowork/Itemadd.java
@@ -11,8 +11,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,22 +41,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
-    GoogleApiClient googleApiClient;
-    FusedLocationProviderApi fusedLocationProviderApi;
-    Location location;
-    //Marker marker;
-    String resultAddress;
-    double resultLat;
-    double resutlLng;
+    Spinner spinner;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
 
     GoogleMap map;
     Intent intent;
     Button tos,reset,regist;
-    EditText item_detail,end_po;
-    TextView start_po;
+    EditText item_detail;
+    TextView start_po,end_po;
     View.OnClickListener cl;
 
     @Override
@@ -66,10 +66,30 @@ public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
         regist = (Button) findViewById(R.id.regist);
 
         start_po = (TextView) findViewById(R.id.start_po);
-        end_po = (EditText) findViewById(R.id.end_po);
+        end_po = (TextView) findViewById(R.id.end_po);
         item_detail = (EditText) findViewById(R.id.item_detail);
 
         ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.main_map)).getMapAsync(this);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("문서");
+        arrayList.add("음식");
+
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,arrayList);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         cl = new View.OnClickListener() {
             @Override
@@ -102,17 +122,36 @@ public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
                                     markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_start));
                                     markerOptions.position(latLng);
                                         map.addMarker(markerOptions);
-                                        Toast toast1 = Toast.makeText(getApplicationContext(), "추가", Toast.LENGTH_SHORT);
-                                        toast1.show();
-                                        map.setOnMapClickListener(null);
+                                    MyGeocodingThread thread=new MyGeocodingThread(latLng);
+                                    thread.start();
 
+                                    map.setOnMapClickListener(null);
                                 }
                             });
                         break;
+                    case R.id.end_po:
+                        Toast toast1 = Toast.makeText(getApplicationContext(),"클릭",Toast.LENGTH_SHORT);
+                        toast1.show();
+
+                        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                            @Override
+                            public void onMapClick(LatLng latLng) {
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker));
+                                markerOptions.position(latLng);
+                                map.addMarker(markerOptions);
+                                MyGeocodingThread2 thread=new MyGeocodingThread2(latLng);
+                                thread.start();
+                                map.setOnMapClickListener(null);
+                            }
+                        });
+                        break;
+
                 }
             }
         };
         start_po.setOnClickListener(cl);
+        end_po.setOnClickListener(cl);
         tos.setOnClickListener(cl);
         reset.setOnClickListener(cl);
         regist.setOnClickListener(cl);
@@ -126,11 +165,11 @@ public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
             CameraPosition position=new CameraPosition.Builder()
                     .target(latLng).zoom(16f).build();
             map.moveCamera(CameraUpdateFactory.newCameraPosition(position));
-            MyGeocodingThread thread=new MyGeocodingThread(latLng);
-            thread.start();
+
 
 
     }
+
     class MyGeocodingThread extends Thread {
         LatLng latLng;
         public MyGeocodingThread(LatLng latLng){
@@ -158,6 +197,8 @@ public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
                     msg.what=100;
                     msg.obj=addressText;
                     handler.sendMessage(msg);
+
+
                 }
             }catch (Exception e){
                 e.printStackTrace();
@@ -165,16 +206,61 @@ public class Itemadd extends AppCompatActivity implements OnMapReadyCallback {
         }
     }
 
+    class MyGeocodingThread2 extends Thread {
+        LatLng latLng;
+        public MyGeocodingThread2(LatLng latLng){
+            this.latLng=latLng;
+        }
+
+        @Override
+        public void run() {
+            Geocoder geocoder=new Geocoder(Itemadd.this);
+            List<Address> addresses=null;
+            String addressText="";
+            try{
+                addresses=geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                Thread.sleep(500);
+                if(addresses != null && addresses.size()>0){
+                    Address address=addresses.get(0);
+                    addressText=address.getAdminArea()+" "+(address.getMaxAddressLineIndex()>0 ?
+                            address.getAddressLine(0) : address.getLocality())+" ";
+                    String txt=address.getSubLocality();
+                    if(txt != null)
+                        addressText += txt+" ";
+                    addressText += address.getThoroughfare()+ " "+address.getSubThoroughfare();
+
+                    Message msg=new Message();
+                    msg.what=200;
+                    msg.obj=addressText;
+                    handler.sendMessage(msg);
+
+
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case 100:
+                    start_po.setText((String)msg.obj);
+                    start_po.setEnabled(false);
                     Toast toast=Toast.makeText(Itemadd.this, (String)msg.obj, Toast.LENGTH_SHORT);
                     toast.show();
                     break;
+                case 200:
+                    end_po.setText((String)msg.obj);
+                    end_po.setEnabled(false);
+                    break;
+
 
             }
         }
     };
+
 }
