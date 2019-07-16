85263e4a8467e2368408742214659068dcee108d
kjy <aty753@naver.com>
2019-07-16 16:38:48 +0900

    메인화면 등록 물품 확인가능

--- app/src/main/java/com/example/waytowork/MainAdapter.java
+++ app/src/main/java/com/example/waytowork/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.example.waytowork;
 
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +16,20 @@ import java.util.ArrayList;
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHolder> {
 
-   private ArrayList<MainData> arrayList;//Main에 리스트 뷰를 담을 공간
+    private ArrayList<MainData> arrayList;//Main에 리스트 뷰를 담을 공간
 
-    public MainAdapter(ArrayList<MainData> arrayList) {
-        this.arrayList = arrayList;
+    private LayoutInflater mInflate; // 추가
+    private Context mContext; // 추가
+    public MainAdapter(Context context, ArrayList<MainData> items){
+        this.arrayList = items;
+        this.mInflate = LayoutInflater.from(context);
+        this.mContext = context;
     }
 
     @NonNull
     @Override
     public MainAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {//액티비티의 온크에이틀당 비슷한대 리스트뷰가 처음으로 생성될때 생성주기
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        View view = mInflate.inflate(R.layout.item_list, parent, false);
         CustomViewHolder holder = new CustomViewHolder(view);
         return holder;
     }
@@ -32,15 +37,16 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHold
     @Override
     public void onBindViewHolder(@NonNull final MainAdapter.CustomViewHolder holder, int position) {
     //실제 추가될때의 생명주기
-        holder.iv_pofile.setImageResource(arrayList.get(position).getIv_profile());
-        holder.tv_name.setText(arrayList.get(position).getTv_name());
-        holder.tv_context.setText(arrayList.get(position).getTv_content());
+        //holder.iv_pofile.setImageResource(arrayList.get(position).getIv_profile());
+        holder.tv_start.setText(arrayList.get(position).tv_start_po);
+        holder.tv_end.setText(arrayList.get(position).tv_end_po);
+        holder.tv_context.setText(arrayList.get(position).tv_content);
 
         holder.itemView.setTag(position);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String curName= holder.tv_name.getText().toString();//현재이름값은
+                String curName= holder.tv_context.getText().toString();//현재내용?
                 Toast.makeText(v.getContext(), curName, Toast.LENGTH_LONG).show();
             }
         });
@@ -58,7 +64,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHold
 
     @Override
     public int getItemCount() {
-        //return 0;
+        //return arrayList.size();
               return (null != arrayList ? arrayList.size() :0);
 
     }
@@ -80,14 +86,16 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHold
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-        protected ImageView iv_pofile;
-        protected TextView tv_name;
+       // protected ImageView iv_pofile;
+        protected TextView tv_start;
+        protected TextView tv_end;
         protected TextView tv_context;
 
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.iv_pofile = (ImageView) itemView.findViewById(R.id.iv_profile);//find뷰랑 비슷함쓰는이유는 액티비티 형태의 클래스가 아니가 떄문에
-            this.tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+           // this.iv_pofile = (ImageView) itemView.findViewById(R.id.iv_profile);//find뷰랑 비슷함쓰는이유는 액티비티 형태의 클래스가 아니가 떄문에
+            this.tv_start = (TextView) itemView.findViewById(R.id.tv_start);
+            this.tv_end = (TextView) itemView.findViewById(R.id.tv_end);
             this.tv_context = (TextView) itemView.findViewById(R.id.tv_content);
         }
     }
